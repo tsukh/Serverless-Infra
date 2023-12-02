d7ce109108d197758ea6ab470bc767e4456226a3
tsukh <144965103+tsukh@users.noreply.github.com>
2023-12-02 17:33:03 -0500

    script.js

--- website/script.js
+++ website/script.js
@@ -28,7 +28,7 @@ document.getElementById('contact-form').addEventListener('submit', function(even
 
 function submitForm(formData) {
   // Make an API request to the backend (API Gateway) for form submission
-  fetch('your-api-gateway', { // URL that represents the backend API endpoint to which the form data is going to be sent
+  fetch('https://bhqdygm5f3.execute-api.us-east-1.amazonaws.com/dev', { // URL that represents the backend API endpoint to which the form data is going to be sent
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
